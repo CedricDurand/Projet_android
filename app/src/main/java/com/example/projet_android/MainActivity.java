452675f6874de45ba5c0e6144af1a3250ef05d19
CedricDurand <cedricdurand13@sfr.fr>
2019-04-02 22:20:59 +0200

    Début implémentation de la popup en fonction de la dernière connexion.

--- app/src/main/java/com/example/projet_android/MainActivity.java
+++ app/src/main/java/com/example/projet_android/MainActivity.java
@@ -1,7 +1,18 @@
 package com.example.projet_android;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +20,55 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button_login =  (Button) findViewById(R.id.btn_login);
+        button_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String strThatDay = "2019/02/23";
+                SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd");
+                Date d = null;
+                try {
+                    d = formatter.parse(strThatDay);//catch exception
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                Calendar thatDay = Calendar.getInstance();
+                thatDay.setTime(d);
+                Calendar today = Calendar.getInstance();
+                long diff = today.getTimeInMillis() - thatDay.getTimeInMillis();
+                long days = diff / (24 * 60 * 60 * 1000);
+                System.out.println("diff "+days);
+
+                if(days>10 || days <= 15 ){
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    builder.setCancelable(true);
+                    builder.setTitle("Alerte");
+                    builder.setMessage("Votre mot de passe va expirer ! Le changer ? ");
+                    builder.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+                         @Override
+                         public void onClick(DialogInterface dialog, int which) {
+
+                         }});
+                    builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                        }
+                    });
+
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+
+
+                }else if(days > 15){
+
+                }
+            }
+        });
+
+
+
+
+
     }
 }
