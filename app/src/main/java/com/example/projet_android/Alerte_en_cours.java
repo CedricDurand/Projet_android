e40a3c0b3b62596e5d25207b9c66e836139d0dd4
CedricDurand <cedricdurand13@sfr.fr>
2019-05-21 01:08:33 +0200

    Réalisation des interfaces sur les alertes. Modification interface sur d'autre activité. Activité sur les alertes fini done.

--- app/src/main/java/com/example/projet_android/Alerte_en_cours.java
+++ app/src/main/java/com/example/projet_android/Alerte_en_cours.java
@@ -2,6 +2,7 @@ package com.example.projet_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Alerte_en_cours extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class Alerte_en_cours extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alerte_en_cours);
+
+        String log = getIntent().getStringExtra("log");
+        String date = getIntent().getStringExtra("date");
+
+        TextView logT = findViewById(R.id.log_en_cours);
+        logT.setText(log);
+
+        TextView dateT = findViewById(R.id.date_en_cours);
+        dateT.setText(date);
     }
 }
--- app/src/main/java/com/example/projet_android/Alerte_fini.java
+++ app/src/main/java/com/example/projet_android/Alerte_fini.java
@@ -2,6 +2,7 @@ package com.example.projet_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Alerte_fini extends AppCompatActivity {
 
@@ -9,5 +10,19 @@ public class Alerte_fini extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alerte_fini);
+
+        String log = getIntent().getStringExtra("log");
+        String date = getIntent().getStringExtra("date");
+        String action = getIntent().getStringExtra("action");
+
+        TextView logT = findViewById(R.id.log_fini);
+        logT.setText(log);
+
+        TextView dateT = findViewById(R.id.date_fini);
+        dateT.setText(date);
+
+        TextView actionT = findViewById(R.id.action_fini);
+        actionT.setText(action);
+
     }
 }
